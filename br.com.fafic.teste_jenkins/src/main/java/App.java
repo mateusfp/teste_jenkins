21d83d6163d5d0e650d2a1f73f3f40ad7d87617a
Francisco Mateus Santos Pinto <ms825957@gmail.com>
2020-11-24 22:25:30 -0200

    primeiro commit

--- br.com.fafic.teste_jenkins/src/main/java/App.java
+++ br.com.fafic.teste_jenkins/src/main/java/App.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Familia
- */
 public class App {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+     
     
 }
